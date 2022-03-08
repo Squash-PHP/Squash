6c634f570fef57591510ebe45b29a790fa6e94e2
Rhys <72263189+givinghawk@users.noreply.github.com>
2022-03-08 21:48:04 +0000

    Update and make psr4 autoload work

--- src/squash.php
+++ src/squash.php
@@ -1,10 +1,21 @@
 <?
-namespace Squash\Squash {
 class Squash {
-function sleepms(int $ms) {
-  $micro = $ms * 1000;
-  return usleep($micro);
-}
+  function generateRandomString($length = 25) {
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+  }
+  function get_json($url) {
+    return json_decode(file_get_contents($url));
+  }
+  function sleepms(int $ms) {
+    $micro = $ms * 1000;
+    return usleep($micro);
+  }
 function files_in_dir($dir, $newline) {
   $fileList = glob($dir.'/*');
   if ($newline == true) {
@@ -25,21 +36,26 @@ function file_in_dir($filename, $dir) {
     return false;
   }
 }
+  function uuid() {
+    return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        // 32 bits for "time_low"
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+
+        // 16 bits for "time_mid"
+        mt_rand( 0, 0xffff ),
+
+        // 16 bits for "time_hi_and_version",
+        // four most significant bits holds version number 4
+        mt_rand( 0, 0x0fff ) | 0x4000,
+
+        // 16 bits, 8 bits for "clk_seq_hi_res",
+        // 8 bits for "clk_seq_low",
+        // two most significant bits holds zero and one for variant DCE1.1
+        mt_rand( 0, 0x3fff ) | 0x8000,
+
+        // 48 bits for "node"
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+    );
 }
-  $Squash = new Squash;
-  //Squash_start();
-//class A {
-  //public $hello;
-
-  //function __construct() {
-  //  $this->hello = new B();
-  //}
-//}
-
-//class B {
-  //public function hello() {
-  //  echo 'IT WORKS';
-  //}
-//}
 }
 ?>
