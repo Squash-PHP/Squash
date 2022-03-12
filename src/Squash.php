db2c5f92ae6c294fbb5c84d3cb176955e8cb3b8d
Rhys <72263189+givinghawk@users.noreply.github.com>
2022-03-12 18:16:49 +0000

    Update Squash.php

--- src/Squash.php
+++ src/Squash.php
@@ -1,5 +1,5 @@
 <?
-$version = "0.0.3";
+$version = "0.0.4";
 $response = json_decode(file_get_contents("https://api.squash.ml/checkver.php?current=$version"));
 if ($response->upgrade == "not needed") {
 unset($response);
