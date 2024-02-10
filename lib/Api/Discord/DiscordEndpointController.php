af64de47c85b384e86b37d41be3e34e74d5866c6
Givinghawk <giveinghawk@gmail.com>
2024-02-10 21:48:28 +0000

    Refactor DiscordEndpointController to improve readability and error handling

--- lib/Api/Discord/DiscordEndpointController.php
+++ lib/Api/Discord/DiscordEndpointController.php
@@ -4,23 +4,39 @@ namespace Squash\Api\Discord;
 
 use Squash\Contract\Api\DiscordEndpointInterface;
 
-final class DiscordEndpointController implements DiscordEndpointInterface {
-    public function sendWebhookMessage(string $message, string $webhookUrl): void {
-        $handle = curl_init();
-        curl_setopt_array($handle, [
-                CURLOPT_URL            => $webhookUrl,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_MAXREDIRS      => 10,
-                CURLOPT_TIMEOUT        => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST  => "POST",
-                CURLOPT_POSTFIELDS     => json_encode(['content' => $message]),
-                CURLOPT_HTTPHEADER     => [
-                        'Content-Type: application/json',
-                ],
-        ]);
-        curl_exec($handle);
-        curl_close($handle);
+final class DiscordEndpointController implements DiscordEndpointInterface
+{
+    /**
+     * Sends a message to a Discord webhook.
+     *
+     * @param string $message The message to send.
+     * @param string $webhookUrl The URL of the Discord webhook.
+     * @param array|null $additionalData Optional. Additional data to send with the message.
+     *
+     * @return void
+     *
+     * @throws \Exception If there is an error executing the cURL request.
+     */
+     
+    public function sendWebhookMessage(string $message, string $webhookUrl, array $additionalData = null): void
+    {
+        /* Thank you to https://gist.github.com/Mo45/cb0813cb8a6ebcd6524f6a36d4f8862c for the original code */
+
+        $json_data = json_encode([
+            "content" => $message,
+        ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+
+
+        $ch = curl_init($webhookUrl);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $response = curl_exec($ch);
+        echo $response;
+        curl_close($ch);
     }
 }
